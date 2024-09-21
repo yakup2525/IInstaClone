304ab36d05055f6501c4dbd0ab767c5facd06d75
yakup2525 <yakup_okumus@outlook.com>
2024-09-21 18:46:46 +0300

    Initial Commit

---
+++ IInstaClone/IInstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  IInstaCloneApp.swift
+//  IInstaClone
+//
+//  Created by Wolf Trail on 21.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct IInstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
